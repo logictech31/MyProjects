92086b0c6c966eea421f67a4641768f8aff4e1d7
logictech31 <hrushikeshbusiness@gmail.com>
2023-07-22 20:38:45 +0530

    Namaei Logic works succesfully!

--- DSA/namei_splint.c
+++ DSA/namei_splint.c
@@ -17,7 +17,6 @@ typedef struct node {
 node* add_new_node(char *data) {
     node *tmp;
     tmp = NEW;
-    int i = 0;
 
     tmp->next = NULL;
     strcpy(tmp->path, data);
@@ -37,7 +36,7 @@ void traverse(node *head) {
 
 void convert_to_ll(node* tmp, char *base_path, char *holder) {
     char *holder_c = holder;
-    
+
     while(*base_path != '\0') {
         if(*base_path == '/') {
             base_path++;
@@ -53,8 +52,52 @@ void convert_to_ll(node* tmp, char *base_path, char *holder) {
     }
 }
 
+int string_compare(char *s1, char *s2) {
+    int flag = 0;
+    while (*s1 != '\0' && *s2 != '\0') {
+        if(*s1 == *s2) {
+            flag = 1;
+        } else {
+            flag = 0;
+            break;
+        }
+        s1++;
+        s2++;
+    }
+
+    return flag;
+}
+
+void namaei_logic(node *h1, node *h2) {
+    node *t1, *t2;
+
+    t1 = h1;
+    t2 = h2;
+    while (t1->next != NULL) {
+        t1 = t1->next;
+    }
+
+    free(t2->prev);
+    while (t2->next != NULL) {
+        t2 = t2->next;
+        if(string_compare(t2->path, "..")) {
+            t1 = t1->prev;
+            free(t1->next);
+            t1->next = NULL;
+        } else if(*(t2->path) == '.' && *(t2->path+sizeof(char)) == '\0') {
+            continue;
+        } else {
+            t1->next = add_new_node(t2->path);
+            t1->next->prev = t1;
+            t1 = t1->next;
+            t1->next = NULL;
+        }
+    }
+    
+}
+
 int main() {
-    node *head, *tmp;
+    node *head, *tmp, *head_2;
     char *base_path, *holder, *holder_c, *rel_path, *rel_path_c;
 
     holder = (char*) malloc(MAX * sizeof(char));
@@ -62,8 +105,9 @@ int main() {
     base_path = (char*) malloc(MAX * sizeof(char));
     rel_path = (char*) malloc(MAX * sizeof(char));
     rel_path_c = rel_path;
-    strcpy(rel_path, ".././usr/data/");
+    strcpy(rel_path, ".././hi/gorakhpur/");
     head = NEW;
+    head_2 = NEW;
     tmp = head;
     tmp->next = NULL;
     tmp->prev = NULL;
@@ -86,6 +130,13 @@ int main() {
         }
     }
 */
+    memset(holder, '\0', MAX * sizeof(char));
+    convert_to_ll(head_2, rel_path, holder);
+    traverse(head);
+    printf("\n\n");
+    traverse(head_2);
+
+    namaei_logic(head, head_2);
     traverse(head);
 
     return 0;
