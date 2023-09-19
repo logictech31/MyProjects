b36849f2d396105ebb807fee99fd4c02361bf38e
logictech31 <hrushipandit13@gmail.com>
2023-09-19 22:40:33 +0530

    moved some files

---
+++ Practice_misc/ChatApp/main.c
@@ -0,0 +1,5 @@
+#include "necessary_headers.h"
+
+int main() {
+    return 0;
+}
---
+++ Practice_misc/ChatApp/necessary_headers.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pthread.h>
