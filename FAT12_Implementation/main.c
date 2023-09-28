2eed3b1a467a222bf663d7a8edcfc20cdb777821
logictech31 <hrushipandit13@gmail.com>
2023-09-29 01:04:48 +0530

    Added a makefile to FAT12_Implementation folder for ease of compiling

--- FAT12_Implementation/main.c
+++ FAT12_Implementation/main.c
@@ -4,25 +4,43 @@ int main(void) {
     FILE *fp;
     struct bootloader bt;
 
-    fp = fopen("fat_sample.bin", "rb");
+    fp = fopen("sample_fat.bin", "rb");
     if(fp != NULL) {
         fread(&bt, 512, 1, fp);
-        fprintf(stdout, "Boot signature: %c\n", bt.boot_signature);
-        fprintf(stdout, "Bytes per sector: %d\n", bt.bytes_per_sector);
-        fprintf(stdout, "Sector per cluster: %d\n", bt.sector_per_clustor);
-        fprintf(stdout, "Number of reserved sectors: %d\n", bt.num_of_reserved_sectors);
-        fprintf(stdout, "Num_of_FATs: %d\n", bt.num_of_FATs);
-        fprintf(stdout, "Max number of root directory entires: %d\n", bt.max_number_of_root_directory_entries);
-        fprintf(stdout, "Total sector count: %d\n", bt.total_sector_count);
-        fprintf(stdout, "Sector per FAT: %d\n", bt.sector_per_FAT);
-        fprintf(stdout, "Sector per track: %d\n", bt.sector_per_track);
-        fprintf(stdout, "Number of heads: %d\n", bt.num_of_heads);
-        fprintf(stdout, "Total_sector_count_F12: %d\n", bt.total_sector_count_f12);
-        fprintf(stdout, "Boot Signature: %c\n", bt.boot_signature);
-        fprintf(stdout, "Volume ID: %d\n", bt.volume_id);
-        fprintf(stdout, "Volume Label: %s", bt.volume_label);
+
+        fprintf(stdout, "Ignore: %s (%ld)\n", bt.ignore1, sizeof(bt.ignore1));
+        fprintf(stdout, "Bytes per sector: %d (%ld)\n", bt.bytes_per_sector, 
+        sizeof(bt.bytes_per_sector));
+        fprintf(stdout, "Sector per cluster: %d (%ld)\n", bt.sector_per_clustor, 
+        sizeof(bt.sector_per_clustor));
+        fprintf(stdout, "Number of reserved sectors: %d (%ld)\n", bt.num_of_reserved_sectors, 
+        sizeof(bt.num_of_reserved_sectors));
+        fprintf(stdout, "Num_of_FATs: %d (%ld)\n", bt.num_of_FATs, 
+        sizeof(bt.num_of_FATs));
+        fprintf(stdout, "Max number of root directory entires: %d (%ld)\n", 
+        bt.max_number_of_root_directory_entries, sizeof(bt.max_number_of_root_directory_entries));
+        fprintf(stdout, "Total sector count: %d (%ld)\n", bt.total_sector_count, 
+        sizeof(bt.total_sector_count));
+        fprintf(stdout, "Ignore 2: %c (%ld)\n", bt.ignore2, sizeof(bt.ignore2));
+        fprintf(stdout, "Sector per FAT: %d (%ld)\n", bt.sector_per_FAT, 
+        sizeof(bt.sector_per_FAT));
+        fprintf(stdout, "Sector per track: %d (%ld)\n", bt.sector_per_track, 
+        sizeof(bt.sector_per_track));
+        fprintf(stdout, "Number of heads: %d (%ld)\n", bt.num_of_heads, 
+        sizeof(bt.num_of_heads));
+        fprintf(stdout, "Ignore 3: %s (%ld)\n", bt.ignore3, sizeof(bt.ignore3));
+        fprintf(stdout, "Total_sector_count_F12: %d (%ld)\n", bt.total_sector_count_f12, 
+        sizeof(bt.total_sector_count_f12));
+        fprintf(stdout, "Ignore 4: %d (%ld)\n", bt.ignore4, sizeof(bt.ignore4));
+        fprintf(stdout, "Boot Signature: %c (%ld)\n", bt.boot_signature, 
+        sizeof(bt.boot_signature));
+        fprintf(stdout, "Volume ID: %d (%ld)\n", bt.volume_id, 
+        sizeof(bt.volume_id));
+        fprintf(stdout, "Volume Label: %s (%ld)", bt.volume_label, 
+        sizeof(bt.volume_label));
         fprintf(stdout, "\n");
-        fprintf(stdout, "File System Type: %s", bt.file_sys_type);
+        fprintf(stdout, "File System Type: %s (%ld)\n", bt.file_sys_type, sizeof(bt.file_sys_type));
+        fprintf(stdout, "Ignore rest: %s (%ld)", bt.ignore_rest, sizeof(bt.ignore_rest));
         // mandatory newline for neat output
         fprintf(stdout, "\n");
     } else {
