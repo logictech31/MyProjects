3e6d53794c2352fa2298d1aec05cb83bbb3ad4ca
logictech31 <hrushipandit13@gmail.com>
2024-02-26 00:50:45 +0530

    Added code from ellipses to understand how it works... i.e. basics of architecture of the printf function

--- DSA/btree.cxx
+++ DSA/btree.cxx
@@ -8,101 +8,117 @@ A B tree of order m contains all the properties of an M way tree. In addition, i
     All leaf nodes must be at the same level.
 
 */
-namespace tree {
+namespace tree
+{
     // Basic node structure of the b-tree
-    class node {
+    class node
+    {
     private:
         unsigned int data;
         struct node *left, *right;
+
     public:
         node();
         ~node();
     };
 
-    node::node() {
+    node::node()
+    {
         this->left = this->right = NULL;
     }
 
-    node::~node() {
+    node::~node()
+    {
         this->left = this->right = NULL;
     }
-
-    class btree {
-        private:
+    // The B-Tree
+    class btree
+    {
+    private:
         // The order of the btree tree
         const int M = 3;
         class node tree_node[3];
-        static node *head;
+        node *head;
 
-        public:
-            // Constructor
-            btree();
-            // Will be used for insertion
-            void operator>>(unsigned int data);
-            // Will be used for searching
-            int operator<<(unsigned int search_key);
-            // Will be used for traversing
-            node *operator++();
-            // Will be used to delete an entry in a node
-            void delete_entry();
-            // Destructor
-            ~btree();
+    public:
+        // Constructor
+        btree();
+        // Will be used for insertion
+        void operator>>(unsigned int data);
+        // Will be used for searching
+        int operator<<(unsigned int search_key);
+        // Will be used for traversing
+        node *operator++();
+        // Will be used to delete an entry in a node
+        void delete_entry();
+        // Destructor
+        ~btree();
+
+    friend void compare_and_assign(node *arr);
     };
 
+    void compare_and_assign(node *arr) {
+    }
+
     btree::btree() {
-        head = &tree_node[0];
+        this->head = &tree_node[0];
     }
 
     btree::~btree() {
-        head = NULL;
+        this->head = NULL;
     }
-/*
-Searching in B Trees is similar to that in Binary search tree. For example, if we search for an item 49 in the following B Tree. The process will something like following :
+    /*
+    Searching in B Trees is similar to that in Binary search tree. For example, if we search for an item 49 in the following B Tree. The process will something like following :
 
-    1. Compare item 49 with root node 78. since 49 < 78 hence, move to its left sub-tree.
-    2. Since, 40<49<56, traverse right sub-tree of 40.
-    3. 49>45, move to right. Compare 49.
-    4. match found, return.
+        1. Compare item 49 with root node 78. since 49 < 78 hence, move to its left sub-tree.
+        2. Since, 40<49<56, traverse right sub-tree of 40.
+        3. 49>45, move to right. Compare 49.
+        4. match found, return.
 
-*/
-    int btree::operator<<(unsigned int search_key) {
+    */
+    int btree::operator<<(unsigned int search_key)
+    {
         int found = -1;
         node *start = head;
 
         return found;
     }
-/*
-Insertions are done at the leaf node level. The following algorithm needs to be followed in order to insert an item into B Tree.
+    /*
+    Insertions are done at the leaf node level. The following algorithm needs to be followed in order to insert an item into B Tree.
 
-    1. Traverse the B Tree in order to find the appropriate leaf node at which the node can be inserted.
-    2. If the leaf node contain less than m-1 keys then insert the element in the increasing order.
-    3. Else, if the leaf node contains m-1 keys, then follow the following steps.
-        - Insert the new element in the increasing order of elements.
-        - Split the node into the two nodes at the median.
-        - Push the median element upto its parent node.
-        - If the parent node also contain m-1 number of keys, then split it too by following the same steps.
+        1. Traverse the B Tree in order to find the appropriate leaf node at which the node can be inserted.
+        2. If the leaf node contain less than m-1 keys then insert the element in the increasing order.
+        3. Else, if the leaf node contains m-1 keys, then follow the following steps.
+            - Insert the new element in the increasing order of elements.
+            - Split the node into the two nodes at the median.
+            - Push the median element upto its parent node.
+            - If the parent node also contain m-1 number of keys, then split it too by following the same steps.
 
 
-*/
-    void btree::operator>>(unsigned int data) {
-        
+    */
+    void btree::operator>>(unsigned int data)
+    {
         return;
     }
 
-/*
-    I will be performing the traversal by using the post increment operator
-    and return the address of the array of order M    
-*/
+    /*
+        I will be performing the traversal by using the post increment operator
+        and return the address of the array of order M
+    */
 }
 
-int main(void) {
+int main(void)
+{
     tree::btree obj;
     unsigned int data_enter_iter = 0, data = 0;
 
     std::cout << "How many times do you want to enter data?\n";
     std::cin >> data_enter_iter;
 
-    while(data_enter_iter--) {
+    std::cout << "Start entering: ";
+
+    while (data_enter_iter--)
+    {
         std::cin >> data;
         obj >> data;
     }
