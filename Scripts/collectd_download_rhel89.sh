53ed1c5ec8d4dd333822a8fff7108e4bcf61527d
logictech31 <hrushikeshbusiness@gmail.com>
2024-04-18 21:19:40 +0530

    Added small changes

--- Scripts/collectd_download_rhel89.sh
+++ Scripts/collectd_download_rhel89.sh
@@ -2,12 +2,15 @@
 if [ $USER == "root" ]
 then
     wget https://storage.googleapis.com/collectd-tarballs/collectd-5.12.0.tar.bz2
-    tar xf collectd-5.12.0.tar.bz2
+    tar -xf collectd-5.12.0.tar.bz2
     cd collectd-5.12.0/
     ./configure
     make all install
     echo "COLLECTD_DIR=\"/root/collectd-5.12.0\"" | tee -a /root/.bashrc
     echo "export PATH=\"$PATH:$COLLECTD_DIR/bin\"" | tee -a /root/.bashrc
+
+    echo "Opening collectd-github, collectd-exporter, lustre-wiki websites"
+    firefox https://github.com/prometheus/collectd_exporter https://www.collectd.org/documentation/ https://wiki.lustre.org/Main_Page
 else
     echo "Run this as root user."
 fi
